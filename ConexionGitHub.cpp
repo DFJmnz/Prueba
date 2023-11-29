9e97b94a98df4847cd08927338f6ca6d5649e4ec
DFJmnz <angela.brcte13@gmail.com>
2023-11-29 12:22:15 -0700

    ISBN 2

--- ConexionGitHub.cpp
+++ ConexionGitHub.cpp
@@ -2,37 +2,13 @@
 
 #include <iostream>
 #include <vector>
+#include <iomanip>  // Necesario para std::setw y std::setfill
 
 using namespace std;
 
 class libro {
-private:
-    string titulo;
-    string autor;
-    string genero;
-    int anioPublic;
-    string ISBN;
-    string review;
-    int stock;
-    int numPaginas;
-    static int numeroLibro
-
-    string generarISBN() {
-        // Obtener el año de publicación con 4 dígitos
-        string anio = to_string(anioPublic);
-        // Obtener el número de libro con 2 dígitos
-        string numLibroStr = to_string(numeroLibro);
-        // Rellenar con ceros a la izquierda si es necesario
-        numLibroStr = std::setw(2) << std::setfill('0') << numLibroStr;
-
-        // Concatenar las partes para formar el ISBN
-        string isbn = "978" + anio + numLibroStr;
-
-        return isbn;
-    }
-
 public:
-    libro(string titulo, string autor, string genero, int anioPublic, 
+    /*libro(string titulo, string autor, string genero, int anioPublic, 
     string ISBN, string review, int stock, int numPaginas) {
         this->titulo = titulo;
         this->autor = autor;
@@ -42,6 +18,15 @@ public:
         this->review = review;
         this->stock = stock;
         this->numPaginas = numPaginas;
+    }*/
+
+    libro(string titulo, string autor, string genero, int anioPublic, 
+        string review, int stock, int numPaginas) 
+        : titulo(titulo), autor(autor), genero(genero), anioPublic(anioPublic),
+          review(review), stock(stock), numPaginas(numPaginas) {
+        // Incrementar el número de libro y generar el ISBN
+        numeroLibro++;
+        ISBN = generarISBN();
     }
 
 //Getters
@@ -76,7 +61,35 @@ int getStock() {
 int getNumPaginas() {
     return numPaginas;
     }
-};
+
+
+private:
+    string titulo;
+    string autor;
+    string genero;
+    int anioPublic;
+    string ISBN;
+    string review;
+    int stock;
+    int numPaginas;
+    static int numeroLibro;
+
+    string generarISBN() {
+        // Obtener el año de publicación con 4 dígitos
+        string anio = to_string(anioPublic);
+        // Obtener el número de libro con 2 dígitos
+        string numLibroStr = to_string(numeroLibro);
+        // Rellenar con ceros a la izquierda si es necesario
+        numLibroStr = std::setw(2) << std::setfill('0') << numLibroStr;
+
+        // Concatenar las partes para formar el ISBN
+        string isbn = "978" + anio + numLibroStr;
+
+        return isbn;
+    }
+
+    int libro::numeroLibro = 0;
+
 
 void imprimirLibros(vector<libro*> v) {
     for(libro* L: v) {
@@ -96,13 +109,13 @@ void imprimirLibros(vector<libro*> v) {
 
 int main() {
 
-libro L1("Caaaaaaa", "Charles Perrault", "Infantil", 1697, 001, "Muy bueno", 5, 20);
-libro L2("Eaaaaaaaaa", "Dr. Seuss", "Infantil", 1971, 002, "Muy bueno", 5, 20);
-libro L3("Daaaaaaaaa", " Miguel de Cervantes", "Aventura", 1605, 003, "Muy bueno", 5, 500);
+    libro L1("Caaaaaaa", "Charles Perrault", "Infantil", 1697, 001, "Muy bueno", 5, 20);
+    libro L2("Eaaaaaaaaa", "Dr. Seuss", "Infantil", 1971, 002, "Muy bueno", 5, 20);
+    libro L3("Daaaaaaaaa", " Miguel de Cervantes", "Aventura", 1605, 003, "Muy bueno", 5, 500);
 
-vector<libro*> v = {&L1, &L2, &L3};
+    vector<libro*> v = {&L1, &L2, &L3};
 
-imprimirLibros(v);
+    imprimirLibros(v);
 
-return 0;
+    return 0;
 }
