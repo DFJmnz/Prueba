62c7475bc94907dc325f49bcc23b2236010d4803
DFJmnz <angela.brcte13@gmail.com>
2023-11-29 12:00:22 -0700

    ISBN 1

--- ConexionGitHub.cpp
+++ ConexionGitHub.cpp
@@ -7,28 +7,44 @@ using namespace std;
 
 class libro {
 private:
-string titulo;
-string autor;
-string genero;
-int anioPublic;
-int ISBN;
-string review;
-int stock;
-int numPaginas;
+    string titulo;
+    string autor;
+    string genero;
+    int anioPublic;
+    string ISBN;
+    string review;
+    int stock;
+    int numPaginas;
+    static int numeroLibro
 
-public:
-libro(string titulo, string autor, string genero, int anioPublic, 
-int ISBN, string review, int stock, int numPaginas) {
-    this->titulo = titulo;
-    this->autor = autor;
-    this->genero = genero;
-    this->anioPublic = anioPublic;
-    this->ISBN = ISBN;
-    this->review = review;
-    this->stock = stock;
-    this->numPaginas = numPaginas;
+    string generarISBN() {
+        // Obtener el año de publicación con 4 dígitos
+        string anio = to_string(anioPublic);
+        // Obtener el número de libro con 2 dígitos
+        string numLibroStr = to_string(numeroLibro);
+        // Rellenar con ceros a la izquierda si es necesario
+        numLibroStr = std::setw(2) << std::setfill('0') << numLibroStr;
+
+        // Concatenar las partes para formar el ISBN
+        string isbn = "978" + anio + numLibroStr;
+
+        return isbn;
     }
 
+public:
+    libro(string titulo, string autor, string genero, int anioPublic, 
+    string ISBN, string review, int stock, int numPaginas) {
+        this->titulo = titulo;
+        this->autor = autor;
+        this->genero = genero;
+        this->anioPublic = anioPublic;
+        this->ISBN = ISBN;
+        this->review = review;
+        this->stock = stock;
+        this->numPaginas = numPaginas;
+    }
+
+//Getters
 string getTitulo() {
     return titulo;
     }
@@ -80,9 +96,9 @@ void imprimirLibros(vector<libro*> v) {
 
 int main() {
 
-libro L1("Caperucita roja", "Charles Perrault", "Infantil", 1697, 001, "Muy bueno", 5, 20);
-libro L2("El lorax", "Dr. Seuss", "Infantil", 1971, 002, "Muy bueno", 5, 20);
-libro L3("Don Quijote de la Mancha", " Miguel de Cervantes", "Aventura", 1605, 003, "Muy bueno", 5, 500);
+libro L1("Caaaaaaa", "Charles Perrault", "Infantil", 1697, 001, "Muy bueno", 5, 20);
+libro L2("Eaaaaaaaaa", "Dr. Seuss", "Infantil", 1971, 002, "Muy bueno", 5, 20);
+libro L3("Daaaaaaaaa", " Miguel de Cervantes", "Aventura", 1605, 003, "Muy bueno", 5, 500);
 
 vector<libro*> v = {&L1, &L2, &L3};
 
